aaae15e78f629b247212c957dfcba898690a8544
Felipe Luiz <felipe.andrade33@etec.sp.gov.br>
2021-12-03 19:33:15 -0300

    Incluido segunda aula

--- src/com/letsCode/Main.java
+++ src/com/letsCode/Main.java
@@ -1,8 +1,42 @@
 package com.letsCode;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int num1 = Num(2, 3);
+        int num2 = 2;
+        String name = "çAÃsPsdí";
+        String name1 = null;
+        System.out.println(name);
+        float nota = 7.0F;
+        if (num2 == num1) {
+            String alguma = "coisa";
+            System.out.println(alguma);
+        }
+        final int QUANTIDADE_TENTATIVAS = 0;
+        System.out.println(QUANTIDADE_TENTATIVAS);
+        /*float pi = Float.parseFloat(Math.PI);*/
+        float pi1 = (float) (Math.PI);
+        System.out.println(pi1);
+        WeekDays monday = WeekDays.monday;
+        System.out.println(monday);
+        System.out.println(WeekDays.tuesday.number);
+
     }
+
+    static int Num(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    float Flo(float num1, float num2) {
+        return num1 + num2;
+    }
+
+public enum WeekDays {
+    monday(1), tuesday(2), wednesday(3), thursday(4), friday(5), saturday(6), sunday(7);
+    private final int number;
+    WeekDays(int valor){
+        number = valor;
+    }
+    }
+
 }
